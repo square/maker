e2117fcd83ad5c36bf048f8a0bca6e6ba88f7448
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2021-03-01 17:03:06 -0500

    docs: sync latest versioned release to latest deploy dir

--- build/sync-to-latest/index.js
+++ build/sync-to-latest/index.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const fse = require('fs-extra');
 const branch = require('git-branch');
 const { version } = require('../../package.json');
 
 const branchName = branch.sync();
-const deploy = branchName === 'master' ? version : branchName;
+if (branchName !== 'master') {
+	// only sync versioned releases to latest directory
+	return;
+}
+const deploy = version;
 
 let deployPath = path.resolve('./', '.dist/styleguide', deploy);
 let latestPath = path.resolve('./', '.dist/styleguide/latest');
 
 console.log(deployPath, latestPath);
+
+fse.removeSync(latestPath);
+fse.copySync(deployPath, latestPath);
