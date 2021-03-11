cfdc4cce4c76e9943bf1969e42321b6a363a84fc
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2021-03-11 11:44:10 -0500

    chore: lint fixes

--- build/checkout-master/index.js
+++ build/checkout-master/index.js
@@ -2,24 +2,24 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
 function hasUncommitedChanges(gitStatus) {
-	let stdout = gitStatus.stdout.toLowerCase();
+	const stdout = gitStatus.stdout.toLowerCase();
 	const uncommitedPhrases = [
 		'changes not staged for commit:',
 		'changes to be committed:',
-		'untracked files:'
+		'untracked files:',
 	];
 	return uncommitedPhrases.some(stdout.includes.bind(stdout));
 }
 
 (async function checkoutMasterAndPull() {
-	let gitStatus = await exec('git status');
+	const gitStatus = await exec('git status');
 	if (hasUncommitedChanges(gitStatus)) {
 		throw new Error('cannot switch to master branch if you have uncommitted changes');
 	}
 
 	await exec('git checkout master');
-	let gitPull = await exec('git pull');
+	const gitPull = await exec('git pull');
 	if (gitPull.stderr) {
-		throw new Error(`git pull wrote to stderr: ${gitPull.stderr}`)
+		throw new Error(`git pull wrote to stderr: ${gitPull.stderr}`);
 	}
-})();
+}());
