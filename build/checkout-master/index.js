5babb61c39e8cc9ee87ecfeb90227ccd47fe0f33
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2021-03-11 11:19:00 -0500

    chore: wip checkout-master script

---
+++ build/checkout-master/index.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const fse = require('fs-extra');
+const branch = require('git-branch');
+const origin = require('remote-origin-url');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+function hasUncommitedChanges(gitStatusStdout) {
+	let stdout = gitStatusStdout.toLowerCase();
+	const uncommitedPhrases = [
+		'changes not staged for commit:',
+		'changes to be committed:',
+		'untracked files:'
+	];
+	return uncommitedPhrases.some(stdout.includes.bind(stdout));
+}
+
+(async function ensureDeployDirectory() {
+	let gitStatus = await exec('git status');
+	if (hasUncommitedChanges(gitStatus.stdout)) {
+		throw new Error('cannot switch to master branch if you have uncommitted changes');
+	}
+
+
+	/*
+	// check if git repo has square/maker as its remote origin
+
+	const validOrigins = [
+		'git@github.com:square/maker.git',
+		'git@github.com:square/maker',
+		'https://github.com/square/maker.git',
+		'https://github.com/square/maker',
+	];
+
+	const remoteOrigin = origin.sync(); // returns undefined if no remote origin set
+	let didAddOrigin = false;
+	if (!validOrigins.includes(remoteOrigin)) {
+		if (!didInit) {
+			throw new Error(`you have some git repo in .dist with remote origin ${remoteOrigin} but the deploy script expects one of: ${validOrigins.join(' ')}`);
+		}
+		await exec('git remote add origin git@github.com:square/maker.git');
+		didAddOrigin = true;
+	}
+
+	// fetch remote branches if necessary
+
+	if (didAddOrigin) {
+		await exec('git fetch origin deploys --depth=1');
+	}
+
+	// check if git repo has the deploys branch checked out
+
+	const gitBranch = branch.sync();
+	if (gitBranch !== 'deploys') {
+		await exec('git checkout deploys');
+	}
+	*/
+})();
