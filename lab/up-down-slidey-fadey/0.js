58e3699bf9e6813171c6150befde06bb25b5c844
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2021-03-15 09:35:16 -0400

    docs: update deploys

--- lab/up-down-slidey-fadey/0.js
+++ lab/up-down-slidey-fadey/0.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{96:function(r,e,n){
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{94:function(r,e,n){
 /**
  * chroma.js - JavaScript library for color conversions
  *
