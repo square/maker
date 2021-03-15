dbd5d646bb666b162e47c3183f5c2421930eaec4
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2021-03-15 12:49:02 -0400

    chore: wip

--- .github/dep-version-check/index.js
+++ .github/dep-version-check/index.js
@@ -2,12 +2,16 @@
 
 const fse = require('fs-extra');
 const semver = require('semver');
-// const path = require('path');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+// env var set by CI
+const branchName = process.env.GITHUB_HEAD_REF;
 
 // current working directory is project root
 const branchPackage = fse.readJsonSync('./package.json');
-//console.log(branchPackage);
 
+/* check deps */
 
 function checkDeps(requiredDeps, installedDeps) {
 	let errors = [];
@@ -27,4 +31,40 @@ function checkDeps(requiredDeps, installedDeps) {
 }
 
 // peer deps should be mirrored in dev deps identically
-checkDeps(branchPackage.peerDependencies, branchPackage.devDependencies)
+checkDeps(branchPackage.peerDependencies, branchPackage.devDependencies);
+
+/* checkout master */
+
+function hasUncommitedChanges(gitStatus) {
+	const stdout = gitStatus.stdout.toLowerCase();
+	const uncommitedPhrases = [
+		'changes not staged for commit:',
+		'changes to be committed:',
+		'untracked files:',
+	];
+	return uncommitedPhrases.some(stdout.includes.bind(stdout));
+}
+
+(async function checkoutMasterAndPull() {
+	const gitStatus = await exec('git status');
+	if (hasUncommitedChanges(gitStatus)) {
+		throw new Error('cannot switch to master branch if you have uncommitted changes');
+	}
+
+	await exec('git checkout master');
+	const gitPull = await exec('git pull');
+	if (gitPull.stderr) {
+		throw new Error(`git pull wrote to stderr: ${gitPull.stderr}`);
+	}
+
+	const branchVer = branchPackage.version;
+
+	// read master's package.json
+	const masterPackage = fse.readJsonSync('./package.json');
+	const masterVer = masterPackage.version;
+
+	// if PR branch's version is not > master's version throw error
+	if (!semver.ge(branchVer, masterVer)) {
+		throw new Error(`${branchName} PR has Maker ver ${branchVer} in package.json which is not >${masterVer} in master branch`);
+	}
+}());
