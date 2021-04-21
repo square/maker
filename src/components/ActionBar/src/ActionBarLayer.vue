ef681d0aec7548168bad80d363ed9e52a2a1d400
Ji Sohn <sohn@squareup.com>
2021-04-21 13:12:53 -0400

    docs(actionbar): add comment to css style

--- src/components/ActionBar/src/ActionBarLayer.vue
+++ src/components/ActionBar/src/ActionBarLayer.vue
@@ -113,7 +113,7 @@ export default {
 
 .Action {
 	margin-right: 8px;
-	-webkit-transform: translate3d(0, 0, 0);
+	-webkit-transform: translate3d(0, 0, 0); /* Fixes buttons flickering on mobile devices */
 	filter: drop-shadow(0 15px 10px rgb(0 0 0 / 20%));
 
 	&:last-child {
