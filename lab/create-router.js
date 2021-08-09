2cf0281b6c94d44bb09230dcb9192cb62dabeca5
hiroki osame <osame@squareup.com>
2021-08-09 18:02:53 -0400

    refactor(lab): removed redundant nested root app (#110)

--- lab/create-router.js
+++ lab/create-router.js
@@ -2,7 +2,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Meta from 'vue-meta';
-import App from './App.vue';
 
 Vue.use(Router);
 Vue.use(Meta);
@@ -13,13 +12,7 @@ const importExperiment = (experiment) => () => import(`./experiments/${experimen
 function createRouter() {
 	return new Router({
 		mode: isProduction ? 'hash' : 'history',
-		routes: [
-			{
-				path: '',
-				component: App,
-				children: LAB_EXPERIMENTS,
-			},
-		],
+		routes: LAB_EXPERIMENTS,
 	});
 }
 
