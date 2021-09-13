80ce3b2cce92df5b90995569918b0be2258d4a96
Ashley Tan <20190589+ashjtan@users.noreply.github.com>
2021-09-13 12:25:17 -0700

    feat(text button): TextButton component (#128)
    * feat(text button): init TextButton component
    
    * feat(text button): remove shape prop / border styles
    
    * feat(text button): remove shape prop / border styles button): init readme
    
    * feat(text button): remove shape prop / border styles button): moved examples in styleguide
    
    * feat(text button): fixed readme
    
    * feat(text button): added necessary styles back in
    
    * docs(text button): extracted TextButton into its own example in styleguide)
    
    * docs(text button): added TextButton description
    
    * feat(text button): added 'label required' warning
    
    * fix(text button): add default color
    
    * fix(text button): inherit color if none passed
    
    * feat(notice button)!: remove NoticeButton (#129)
    
    * feat(text button): delete NoticeButton
    
    * fix(notice): fix assert warning logic
    
    * fix(notice): replace notice buttons w/ text buttons
    
    * docs(notice): update api table

---
+++ src/components/Button/src/TextButton.vue
@@ -0,0 +1,183 @@
+<template>
+	<button
+		:class="[
+			$s.Button,
+			$s[`size_${size}`],
+			{
+				[$s.loading]: loading,
+			}
+		]"
+		:type="type"
+		:disabled="disabled"
+		:style="style"
+		v-bind="$attrs"
+		v-on="$listeners"
+	>
+		<m-loading
+			v-if="loading"
+			:class="$s.Loading"
+		/>
+		<span :class="$s.MainText">
+			<!-- @slot Button label -->
+			<slot />
+		</span>
+	</button>
+</template>
+
+<script>
+import chroma from 'chroma-js';
+import { MLoading } from '@square/maker/components/Loading';
+import assert from '@square/maker/utils/assert';
+
+function textButton(tokens) {
+	const textColor = tokens.color ? chroma(tokens.color) : undefined;
+	return {
+		'--color-main': 'color',
+		'--color-contrast': textColor ? textColor.hex() : undefined,
+	};
+}
+
+/**
+ * Button component
+ * @inheritAttrs button
+ * @inheritListeners button
+ */
+export default {
+	components: {
+		MLoading,
+	},
+
+	inheritAttrs: false,
+
+	props: {
+		/**
+		 * Type of the button
+		 */
+		type: {
+			type: String,
+			default: 'button',
+		},
+		/**
+		 * Size of the button label text
+		 */
+		size: {
+			type: String,
+			default: 'medium',
+			validator: (size) => ['medium', 'large'].includes(size),
+		},
+		/**
+		 * Text color of button
+		 */
+		color: {
+			type: String,
+			default: undefined,
+			validator: (color) => chroma.valid(color),
+		},
+		/**
+		 * Toggles button disabled state
+		 */
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		/**
+		 * Toggles button loading state
+		 */
+		loading: {
+			type: Boolean,
+			default: false,
+		},
+	},
+
+	computed: {
+		style() {
+			return textButton({
+				color: this.color,
+			});
+		},
+	},
+
+	created() {
+		assert.warn(this.$slots.default, 'TextButton should only be used with a label');
+	},
+
+};
+</script>
+
+<style module="$s">
+.Button {
+	position: relative;
+	display: inline-flex;
+	align-items: center;
+	min-width: 0;
+	color: var(--color-contrast, inherit);
+	font-weight: 500;
+	font-family: inherit;
+	vertical-align: middle;
+	background-color: var(--color-main);
+	border: none;
+	border-radius: 8px;
+	outline: none;
+	box-shadow:
+		var(--outline-border, 0 0),
+		var(--focus-border, 0 0);
+	cursor: pointer;
+	transition:
+		color 0.2s ease-in,
+		background-color 0.2s ease-in,
+		box-shadow 0.2s ease-in;
+	user-select: none;
+	touch-action: manipulation;
+	fill: currentColor;
+
+	&.iconButton {
+		min-width: max-content;
+	}
+
+	&.size_medium {
+		font-size: 14px;
+	}
+
+	&.size_large {
+		font-size: 16px;
+	}
+
+	&:disabled {
+		cursor: initial;
+
+		& > * {
+			opacity: 0.4;
+		}
+	}
+
+	&.loading {
+		/* don't inherit color in loading state on hover/active */
+		color: transparent !important;
+	}
+}
+
+.Loading {
+	position: absolute;
+	top: 4px;
+	right: 4px;
+	bottom: 4px;
+	left: 4px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: var(--color-contrast);
+	background-color: transparent;
+}
+
+.MainText {
+	display: flex;
+	gap: 8px;
+	align-items: center;
+	width: max-content;
+	max-width: 100%;
+	overflow: hidden;
+	line-height: 1;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+}
+</style>
