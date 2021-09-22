52029766646b9ec1f093754a586813334ee54a8e
tatems <jtate@squareup.com>
2021-09-22 11:33:38 -0400

    feat(progress-bar): create progress bar component and readme

---
+++ src/components/ProgressBar/src/ProgressBar.vue
@@ -0,0 +1,104 @@
+<template>
+	<div
+		:class="[
+			$s.ProgressBarContainer,
+			$s[`size_${size}`],
+			$s[`shape_${shape}`],
+		]"
+	>
+		<div
+			:class="[
+				$s.ProgressBar,
+				$s[`shape_${shape}`],
+			]"
+			:style="{
+				backgroundColor: color,
+				transform: `translateX(-${progressTranslate}%)`
+			}"
+		/>
+	</div>
+</template>
+
+<script>
+import chroma from 'chroma-js';
+
+const MIN_PROGRESS = 0;
+const MAX_PROGRESS = 100;
+
+export default {
+	name: 'ProgressBar',
+
+	props: {
+		size: {
+			type: String,
+			default: 'medium',
+			validator: (size) => ['xsmall', 'small', 'medium', 'large'].includes(size),
+		},
+		shape: {
+			type: String,
+			default: 'rounded',
+			validator: (shape) => ['squared', 'rounded', 'pill'].includes(shape),
+		},
+		color: {
+			type: String,
+			default: '#000',
+			validator: (color) => chroma.valid(color),
+		},
+		progress: {
+			type: Number,
+			default: 0,
+			validator: (progress) => progress >= MIN_PROGRESS && progress <= MAX_PROGRESS,
+		},
+	},
+
+	computed: {
+		progressTranslate() {
+			return MAX_PROGRESS - this.progress;
+		},
+	},
+};
+</script>
+
+<style module="$s">
+.ProgressBarContainer {
+	width: 100%;
+	overflow: hidden;
+	background-color: #f5f4f4;
+}
+
+.ProgressBar {
+	height: 100%;
+	transition: transform 100ms linear;
+}
+
+.ProgressBarContainer.size_xsmall {
+	height: 4px;
+}
+
+.ProgressBarContainer.size_small {
+	height: 8px;
+}
+
+.ProgressBarContainer.size_medium {
+	height: 12px;
+}
+
+.ProgressBarContainer.size_large {
+	height: 16px;
+}
+
+.ProgressBarContainer.shape_squared,
+.ProgressBar.shape_squared {
+	border-radius: 0;
+}
+
+.ProgressBarContainer.shape_rounded,
+.ProgressBar.shape_rounded {
+	border-radius: 8px;
+}
+
+.ProgressBarContainer.shape_pill,
+.ProgressBar.shape_pill {
+	border-radius: 16px;
+}
+</style>
