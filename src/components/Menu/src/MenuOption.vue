06f1886394ee862aef5034b2b587be0f9d3e214e
tatems <jtate@squareup.com>
2021-10-28 14:31:31 -0400

    feat(menu): start styling for menu

--- src/components/Menu/src/MenuOption.vue
+++ src/components/Menu/src/MenuOption.vue
@@ -1,23 +1,40 @@
 <template>
 	<div
-		style="display: flex;"
+		:class="$s.MenuOption"
 		@mousedown.prevent="selectOption"
 	>
-		<slot />
-		<template v-if="isSelected">
-			(Selected)
-		</template>
+		<div :class="$s.MenuOptionIconContainer">
+			<check-icon
+				:class="{
+					[$s.MenuOptionIcon]: true,
+					[$s.isShown]: isSelected
+				}"
+			/>
+		</div>
+		<div :class="$s.MenuOptionIconContent">
+			<slot />
+		</div>
 	</div>
 </template>
 
 <script>
+import CheckIcon from '@square/maker-icons/Check';
+import { MThemeKey, defaultTheme } from '@square/maker/components/Theme';
 import MenuKey from './key';
 
 export default {
 	name: 'MenuOption',
 
+	components: {
+		CheckIcon,
+	},
+
 	inject: {
 		menu: MenuKey,
+		theme: {
+			default: defaultTheme(),
+			from: MThemeKey,
+		},
 	},
 
 	props: {
@@ -41,6 +58,31 @@ export default {
 };
 </script>
 
-<style>
-/* Keep */
+<style module="$s">
+.MenuOption {
+	display: flex;
+	align-items: center;
+	padding: 12px;
+	cursor: pointer;
+}
+
+.MenuOptionIconContainer {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 24px;
+	height: 24px;
+	margin-right: 8px;
+}
+
+.MenuOptionIcon {
+	width: 16px;
+	height: 16px;
+	opacity: 0;
+	transition: opacity 75ms linear;
+
+	&.isShown {
+		opacity: 1;
+	}
+}
 </style>
