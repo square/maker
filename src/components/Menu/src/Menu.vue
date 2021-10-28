27b33371e3f3ae1f67f63ebe19e1f4e8cfebb018
tatems <jtate@squareup.com>
2021-10-28 13:45:10 -0400

    feat(menu): setup inter-component communication w/ selection, update menu update event name

--- src/components/Menu/src/Menu.vue
+++ src/components/Menu/src/Menu.vue
@@ -2,12 +2,12 @@
 	<div>
 		<slot
 			v-for="(option, idx) in options"
-			v-bind="{ option, isSelected: false }"
+			v-bind="option"
 		>
 			<m-menu-option
 				:key="idx"
 				:is-selected="false"
-				:value="option"
+				:option="option"
 			>
 				<m-menu-option-label>
 					{{ option }}
@@ -18,8 +18,10 @@
 </template>
 
 <script>
+import { isEqual } from 'lodash';
 import MMenuOption from './MenuOption.vue';
 import MMenuOptionLabel from './MenuOptionLabel.vue';
+import MenuKey from './key';
 
 export default {
 	name: 'Menu',
@@ -29,14 +31,77 @@ export default {
 		MMenuOptionLabel,
 	},
 
+	provide() {
+		return {
+			[MenuKey]: this.menuAPI,
+		};
+	},
+
+	model: {
+		event: 'menu:update',
+	},
+
 	props: {
 		value: {
 			type: undefined,
-			required: true,
+			default: undefined,
 		},
 		options: {
 			type: Array,
 			required: true,
+		},
+
+		isMultiselect: {
+			type: Boolean,
+			default: false,
+		},
+	},
+
+	data() {
+		return {
+			menuAPI: {
+				selectOption: this.selectOption,
+				isOptionSelected: this.isOptionSelected,
+			},
+		};
+	},
+
+	created() {
+		if (this.isMultiselect && !Array.isArray(this.value)) {
+			if (this.value === undefined) {
+				this.updateValue([]);
+			} else {
+				this.updateValue([this.value]);
+			}
+		}
+	},
+
+	methods: {
+		updateValue(value) {
+			this.$emit('menu:update', value);
+		},
+
+		selectOption(option) {
+			if (this.isMultiselect) {
+				this.updateMultiselectValue(option);
+			} else {
+				this.updateValue(option);
+			}
+		},
+
+		updateMultiselectValue(option) {
+			const filteredValue = this.value.filter((innerValue) => !isEqual(option, innerValue));
+			const newValue = filteredValue.length === this.value.length
+				? [...this.value, option]
+				: filteredValue;
+
+			this.updateValue(newValue);
+		},
+
+		isOptionSelected(option) {
+			return this.isMultiselect
+				? this.value?.some?.((innerValue) => isEqual(option, innerValue))
+				: isEqual(option, this.value);
 		},
 	},
 };
