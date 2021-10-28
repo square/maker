f949a1b32cd7ad0c1405cba0065b68cda422ca6e
tatems <jtate@squareup.com>
2021-10-28 11:20:21 -0400

    feat(menu): setup slots

--- src/components/Menu/src/Menu.vue
+++ src/components/Menu/src/Menu.vue
@@ -1,10 +1,44 @@
 <template>
-	<div>Menu</div>
+	<div>
+		<slot
+			v-for="(option, idx) in options"
+			v-bind="{ option, isSelected: false }"
+		>
+			<m-menu-option
+				:key="idx"
+				:is-selected="false"
+				:value="option"
+			>
+				<m-menu-option-label>
+					{{ option }}
+				</m-menu-option-label>
+			</m-menu-option>
+		</slot>
+	</div>
 </template>
 
 <script>
+import MMenuOption from './MenuOption.vue';
+import MMenuOptionLabel from './MenuOptionLabel.vue';
+
 export default {
-	name: 'MenuOption',
+	name: 'Menu',
+
+	components: {
+		MMenuOption,
+		MMenuOptionLabel,
+	},
+
+	props: {
+		value: {
+			type: undefined,
+			required: true,
+		},
+		options: {
+			type: Array,
+			required: true,
+		},
+	},
 };
 </script>
 
