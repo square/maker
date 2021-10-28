a656e45ce0fd094135a68a201c8fcc45abd3d119
tatems <jtate@squareup.com>
2021-10-28 13:45:10 -0400

    feat(menu): setup inter-component communication w/ selection, update menu update event name

--- src/components/Menu/src/MenuOption.vue
+++ src/components/Menu/src/MenuOption.vue
@@ -1,10 +1,43 @@
 <template>
-	<div><slot /></div>
+	<div
+		style="display: flex;"
+		@mousedown.prevent="selectOption"
+	>
+		<slot />
+		<template v-if="isSelected">
+			(Selected)
+		</template>
+	</div>
 </template>
 
 <script>
+import MenuKey from './key';
+
 export default {
 	name: 'MenuOption',
+
+	inject: {
+		menu: MenuKey,
+	},
+
+	props: {
+		option: {
+			type: undefined,
+			required: true,
+		},
+	},
+
+	computed: {
+		isSelected() {
+			return this.menu.isOptionSelected(this.option);
+		},
+	},
+
+	methods: {
+		selectOption() {
+			this.menu.selectOption(this.option);
+		},
+	},
 };
 </script>
 
