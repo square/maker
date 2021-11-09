25fbd22b6bd0d8575ff346c3046ba348691250a5
Lauren Hu <ruiwen@squareup.com>
2021-11-09 17:35:35 -0500

    feat(modal): updates to modal swipe lab

--- lab/experiments/ActionBarTest/CartModal.vue
+++ lab/experiments/ActionBarTest/CartModal.vue
@@ -1,25 +1,34 @@
 <template>
 	<m-modal>
-		<template
-			#dismiss
+		<div
+			v-if="showImage"
+			class="cover-photo"
 		>
-			<div class="cover-photo">
-				<m-image
-					src="https://i.picsum.photos/id/507/900/900.jpg?hmac=NDltE7xXtFlZjUoyDqGjehzY5ORPtj4-d42qbFgAFkk"
-				/>
-			</div>
-		</template>
-
-		<div>
-			<h1>Cart modal content</h1>
-			<div
-				v-for="i in 100"
-				:key="i"
-			>
-				Long text {{ i }}
-			</div>
+			<m-image
+				src="https://i.picsum.photos/id/507/900/900.jpg?hmac=NDltE7xXtFlZjUoyDqGjehzY5ORPtj4-d42qbFgAFkk"
+			/>
 		</div>
-
+		<m-segmented-control
+			v-else
+			v-model="selected"
+		>
+			<m-segment value="short">
+				Local Delivery
+			</m-segment>
+			<m-segment value="medium">
+				Pickup
+			</m-segment>
+			<m-segment value="long">
+				Ship
+			</m-segment>
+		</m-segmented-control>
+		<h1>Cart modal content</h1>
+		<div
+			v-for="i in 100"
+			:key="i"
+		>
+			Long text {{ i }}
+		</div>
 		<m-inline-action-bar>
 			<m-action-bar-button
 				key="close"
@@ -47,6 +56,7 @@
 import { MModal, modalApi } from '@square/maker/components/Modal';
 import { MInlineActionBar, MActionBarButton } from '@square/maker/components/ActionBar';
 import { MImage } from '@square/maker/components/Image';
+import { MSegmentedControl, MSegment } from '@square/maker/components/SegmentedControl';
 import X from '@square/maker-icons/X';
 
 export default {
@@ -54,11 +64,27 @@ export default {
 		MModal,
 		MInlineActionBar,
 		MActionBarButton,
+		MSegmentedControl,
+		MSegment,
 		MImage,
 		X,
 	},
+
 	inject: {
 		modalApi,
+	},
+
+	props: {
+		showImage: {
+			type: Boolean,
+			default: true,
+		},
+	},
+
+	data() {
+		return {
+			selected: 'short',
+		};
 	},
 };
 </script>
