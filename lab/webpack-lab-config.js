c25f1e6ffa6b7dab037d872c4686b269f5fa34d5
Landon Durnan <durnan@squareup.com>
2021-11-10 16:56:12 -0800

    build: pwa lab environment

--- lab/webpack-lab-config.js
+++ lab/webpack-lab-config.js
@@ -105,6 +105,21 @@ const config = merge({}, webpackBaseConfig, {
 			}),
 		] : []),
 	],
+
+	pwa: {
+		name: 'My App',
+		themeColor: '#4DBA87',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+
+		// configure the workbox plugin
+		workboxPluginMode: 'InjectManifest',
+		workboxOptions: {
+			// swSrc is required in InjectManifest mode.
+			swSrc: 'dev/sw.js',
+		},
+	},
 });
 
 module.exports = config;
