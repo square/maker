4f50fe9cbaedd0db59602a5bbb754be4581f963e
laurenhu <adadochka@gmail.com>
2021-11-17 17:49:48 -0500

    feat(inline-action-bar): delayed spring transition (#184)
    * feat(inline-action-bar): add delayed spring transition to InlineActionBar
    
    * feat(inline-action-bar): remove props from InlineActionBar

---
+++ lab/experiments/ActionBarTransition.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<m-action-bar-layer>
+			<router-view />
+		</m-action-bar-layer>
+		<m-modal-layer />
+	</div>
+</template>
+
+<script>
+import { MActionBarLayer } from '@square/maker/components/ActionBar';
+import { MModalLayer } from '@square/maker/components/Modal';
+
+export default {
+	components: {
+		MActionBarLayer,
+		MModalLayer,
+	},
+
+	mixins: [
+		MModalLayer.apiMixin,
+	],
+};
+</script>
+
+<style>
+html,
+body {
+	font-family: "Square Market", system-ui;
+}
+</style>
--- src/utils/transitions.js
+++ src/utils/transitions.js
@@ -19,6 +19,13 @@ export const spring = {
 	mass,
 };
 
+export const springBounce = {
+	type,
+	stiffness: 400,
+	damping: 30,
+	mass: 2,
+};
+
 const START_VALUE = 0;
 const END_VALUE = 100;
 const MINI_END_VALUE = 40;
@@ -230,6 +237,38 @@ export function floatDownFn({ element, onComplete }) {
 	animate({
 		...animationDirection,
 		...spring,
+		onUpdate(number) {
+			elementStyler.set(styleFn(number));
+		},
+		onComplete,
+	});
+}
+
+export function springUpBounceFn({ element, onComplete }) {
+	const elementStyler = styler(element);
+	const styleFn = toRelativeY;
+	const animationDirection = animateDown;
+	elementStyler.set(styleFn(animationDirection.from));
+	elementStyler.render();
+	animate({
+		...animationDirection,
+		...springBounce,
+		onUpdate(number) {
+			elementStyler.set(styleFn(number));
+		},
+		onComplete,
+	});
+}
+
+export function springDownBounceFn({ element, onComplete }) {
+	const elementStyler = styler(element);
+	const styleFn = toRelativeY;
+	const animationDirection = animateUp;
+	elementStyler.set(styleFn(animationDirection.from));
+	elementStyler.render();
+	animate({
+		...animationDirection,
+		...springBounce,
 		onUpdate(number) {
 			elementStyler.set(styleFn(number));
 		},
