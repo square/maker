3f8e08574547808f0eaec99472d3f5300a6ec50a
Ji Sohn <sohn@squareup.com>
2022-02-03 14:25:23 -0500

    fix(text): typo in line height (#217)

--- src/components/Text/src/Text.vue
+++ src/components/Text/src/Text.vue
@@ -86,7 +86,7 @@ export default {
 
 .Paragraph.size_minus-1 {
 	font-size: var(--font-step-minus-1-size);
-	line-height: var(--font-ste-minus-1-line-height);
+	line-height: var(--font-step-minus-1-line-height);
 }
 
 .Paragraph.size_0 {
