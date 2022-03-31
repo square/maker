181dc0c4ecd27211d11ea915d6258620e1789f9e
kirill <7769424+pretzelhammer@users.noreply.github.com>
2022-03-31 08:50:37 -0400

    fix: double scrollbars on Dialog, Modal, Blade (#273)
    * fix(dialog): double scrollbars
    
    * fix(modal): double scrollbars
    
    * fix(blade): double scrollbars

--- src/components/Dialog/src/Dialog.vue
+++ src/components/Dialog/src/Dialog.vue
@@ -1,12 +1,10 @@
 <template>
 	<div
-		:class="$s.Container"
+		:class="$s.Dialog"
 		:style="style"
 	>
-		<div :class="$s.Dialog">
-			<!-- @slot Dialog content -->
-			<slot />
-		</div>
+		<!-- @slot Dialog content -->
+		<slot />
 	</div>
 </template>
 
@@ -57,31 +55,13 @@ export default {
 </script>
 
 <style module="$s">
-.Container {
-	position: relative;
-	width: 100%;
-	max-height: calc(100% - 48px);
-	overflow: scroll;
-	border-radius: 8px 8px 0 0;
-}
-
 .Dialog {
-	overflow: scroll;
+	overflow: auto;
 	color: var(--color, inherit);
 	background: var(--bg-color, #f5f6f7);
 }
 
 @media screen and (--for-tablet-landscape-up) {
-	.Container {
-		display: inline-block;
-		width: auto;
-		height: auto;
-		max-height: calc(100% - 64px);
-		overflow: hidden;
-		border-radius: 8px;
-		box-shadow: 0 0 24px 8px rgba(0, 0, 0, 0.3);
-	}
-
 	.Dialog {
 		width: 400px;
 		min-height: 180px;
