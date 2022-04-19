81d7e5708e8f403ce35fa3a9155206b0f88eba45
laurenhu <ruiwen@squareup.com>
2022-04-19 12:03:10 -0400

    feat(menu): menu component (#288)
    * feat(row): row component
    
    * feat(menu): menu component
    
    * feat(menu): cleanup for menu component
    
    * fix(menu): linter error
    
    * feat(menu): clean up menu component
    
    * feat(menu): clean up menu component
    
    * feat(menu): add keyboard controls for menu component

---
+++ src/utils/Row/index.js
@@ -0,0 +1,1 @@
+export { default as MRow } from './src/Row.vue';
---
+++ src/utils/Row/src/Row.vue
@@ -0,0 +1,99 @@
+<template>
+	<div
+		:class="$s.Row"
+		v-bind="$attrs"
+		v-on="$listeners"
+	>
+		<div
+			v-if="$slots.prefix"
+			:class="$s.Prefix"
+		>
+			<slot name="prefix" />
+		</div>
+		<div
+			v-if="$slots.default"
+			:class="$s.Label"
+		>
+			<div>
+				<slot />
+			</div>
+			<div
+				v-if="$slots.secondary"
+				:class="$s.Secondary"
+			>
+				<slot name="secondary" />
+			</div>
+		</div>
+		<div
+			v-if="$slots.side"
+			:class="$s.Side"
+		>
+			<div>
+				<slot name="side" />
+			</div>
+			<div
+				v-if="$slots['side-secondary']"
+				:class="$s.Secondary"
+			>
+				<slot name="side-secondary" />
+			</div>
+		</div>
+		<div
+			v-if="$slots['suffix']"
+			:class="$s.Suffix"
+		>
+			<slot name="suffix" />
+		</div>
+	</div>
+</template>
+
+<script>
+
+/**
+ * @inheritAttrs div
+ * @inheritListeners div
+ */
+export default {
+	inheritAttrs: false,
+};
+</script>
+
+<style module="$s">
+.Row {
+	--opacity-sublabel: 0.7;
+
+	display: flex;
+	gap: 8px;
+	align-items: center;
+	width: 100%;
+}
+
+.Label,
+.Side {
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	gap: 4px;
+	font-size: 16px;
+}
+
+.Secondary {
+	font-size: 14px;
+	opacity: var(--opacity-sublabel);
+}
+
+.Prefix,
+.Label {
+	text-align: left;
+}
+
+.Side,
+.Suffix {
+	text-align: right;
+}
+
+.Prefix,
+.Suffix {
+	display: flex;
+}
+</style>
