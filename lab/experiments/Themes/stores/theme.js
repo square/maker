81158f918e95a80f004c71b2632737f4c861d396
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2022-05-10 16:06:19 -0400

    docs: update themes lab colors theme object

--- lab/experiments/Themes/stores/theme.js
+++ lab/experiments/Themes/stores/theme.js
@@ -5,7 +5,7 @@ export const useThemeStore = defineStore('theme', {
 		theme: {
 			colors: {
 				background: '#ffffff',
-				text: '#000000',
+				body: '#000000',
 				heading: '#000000',
 				primary: '#000000',
 			},
