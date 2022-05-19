e0690731cd2a5869aded0ee34ea0aaeee822fb6b
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2022-05-19 08:55:01 -0400

    ci: generate deploy index with versioned lab releases

--- build/generate-deploy-index/index.js
+++ build/generate-deploy-index/index.js
@@ -26,7 +26,7 @@ ensureDirectory(LAB_DIST);
 ensureDirectory(STYLEGUIDE_DIST);
 
 const STYLEGUIDE_DEPLOYS = getDirectories(STYLEGUIDE_DIST).filter((d) => d !== '0.0.0-semantic-release');
-const LAB_DEPLOYS = getDirectories(LAB_DIST);
+const LAB_DEPLOYS = getDirectories(LAB_DIST).filter((d) => d !== '0.0.0-semantic-release');
 const additionalVersions = ['latest-preview', 'latest'];
 
 function isVersion(deployName) {
@@ -35,8 +35,8 @@ function isVersion(deployName) {
 function isntVersion(deployName) {
 	return !isVersion(deployName);
 }
-function sort(items, isNumericVersions) {
-	if (isNumericVersions) {
+function sort(items, isSemverItems) {
+	if (isSemverItems) {
 		items = items.filter((item) => semverValid(item));
 		semverSort(items);
 
@@ -50,8 +50,8 @@ function sort(items, isNumericVersions) {
 	return items.sort();
 }
 
-function toDeployLinks(prefix, suffix, items, isNumericVersions) {
-	items = sort(items, isNumericVersions);
+function toDeployLinks(prefix, suffix, items, isSemverItems) {
+	items = sort(items, isSemverItems);
 	return items.map((item) => `<li><a href="${prefix}${item}${suffix}">${item}</a></li>`).join('\n');
 }
 
@@ -64,9 +64,15 @@ const BUILT_INDEX = `
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		body {
+			font-family: sans-serif;
+		}
+	</style>
 </head>
 <body>
-	<h2>Styleguide Deploys</h2>
+	<h2>Styleguides</h2>
 	<h3>Versioned Releases</h3>
 	<ul>
 		${toDeployLinks(STYLEGUIDE_URL_PREFIX, URL_SUFFIX, STYLEGUIDE_DEPLOYS.filter(isVersion), true)}
@@ -75,9 +81,14 @@ const BUILT_INDEX = `
 	<ul>
 		${toDeployLinks(STYLEGUIDE_URL_PREFIX, URL_SUFFIX, STYLEGUIDE_DEPLOYS.filter(isntVersion))}
 	</ul>
-	<h2>Lab Deploys</h2>
+	<h2>Labs</h2>
+	<h3>Versioned Releases</h3>
 	<ul>
-		${toDeployLinks(LAB_URL_PREFIX, URL_SUFFIX, LAB_DEPLOYS)}
+		${toDeployLinks(LAB_URL_PREFIX, URL_SUFFIX, LAB_DEPLOYS.filter(isVersion), true)}
+	</ul>
+	<h3>WIP Branches</h3>
+	<ul>
+		${toDeployLinks(LAB_URL_PREFIX, URL_SUFFIX, LAB_DEPLOYS.filter(isntVersion))}
 	</ul>
 </body>
 </html>
