f9d446cd398fa3aea53471d1953e775d31031ec4
kirill <7769424+pretzelhammer@users.noreply.github.com>
2022-05-20 15:43:45 -0400

    ci: more improvements (#331)
    * ci: force deploy name by setting optional DEPLOY_NAME env var in styleguide & lab build scripts
    
    * ci: add logging to ci
    
    * ci: improve ci logging
    
    * ci: actually implements remove-styleguide-lab workflow
    
    * ci: rename WIPs to PRs in deploy index
    
    * ci: adds comment to remove-styleguide-lab workflow to describe downside of implementation
    
    * ci: renames latest & latest-preview to latest-stable & latest-beta
    
    * ci: always run sync-to-latest, regardless of whether on semantic release branch or not
    
    * ci: styles deploy index some more
    
    * ci: adds updated timestamp to generated deploy index
    
    * ci: improves automated commented styleguide & lab deploy PR message
    
    * docs: updates github readme

--- build/generate-deploy-index/index.js
+++ build/generate-deploy-index/index.js
@@ -4,6 +4,11 @@ const path = require('path');
 const fs = require('fs');
 const semverSort = require('semver/functions/rsort');
 const semverValid = require('semver/functions/valid');
+const {
+	NO_VERSION_INFO,
+	LATEST_STABLE,
+	LATEST_BETA,
+} = require('../utils');
 
 const DIST = path.resolve(process.cwd(), '.dist');
 const DIST_INDEX = path.resolve(DIST, 'index.html');
@@ -16,34 +21,46 @@ function ensureDirectory(directory) {
 	}
 }
 
+ensureDirectory(LAB_DIST);
+ensureDirectory(STYLEGUIDE_DIST);
+
 function getDirectories(baseDirectory) {
 	return fs.readdirSync(baseDirectory, { withFileTypes: true })
 		.filter((entry) => entry.isDirectory())
 		.map((directory) => directory.name);
 }
 
-ensureDirectory(LAB_DIST);
-ensureDirectory(STYLEGUIDE_DIST);
+function getDeploys(baseDirectory) {
+	return getDirectories(baseDirectory)
+		.filter((deploy) => deploy !== NO_VERSION_INFO);
+}
 
-const STYLEGUIDE_DEPLOYS = getDirectories(STYLEGUIDE_DIST).filter((d) => d !== '0.0.0-semantic-release');
-const LAB_DEPLOYS = getDirectories(LAB_DIST).filter((d) => d !== '0.0.0-semantic-release');
-const additionalVersions = ['latest-preview', 'latest'];
+const STYLEGUIDE_DEPLOYS = getDeploys(STYLEGUIDE_DIST);
+const LAB_DEPLOYS = getDeploys(LAB_DIST);
+const LATEST_ALIASES = new Set([
+	LATEST_STABLE,
+	LATEST_BETA,
+]);
 
 function isVersion(deployName) {
-	return additionalVersions.includes(deployName) || semverValid(deployName);
+	return LATEST_ALIASES.has(deployName) || semverValid(deployName);
 }
 function isntVersion(deployName) {
 	return !isVersion(deployName);
 }
 function sort(items, isSemverItems) {
 	if (isSemverItems) {
-		items = items.filter((item) => semverValid(item));
-		semverSort(items);
-
-		additionalVersions.forEach((version) => {
-			items.unshift(version);
-		});
-
+		const hasLatestStable = items.includes(LATEST_STABLE);
+		const hasLatestBeta = items.includes(LATEST_BETA);
+		const semverOnlyItems = items.filter((item) => semverValid(item));
+		semverSort(semverOnlyItems);
+		items = semverOnlyItems;
+		if (hasLatestBeta) {
+			items.unshift(LATEST_BETA);
+		}
+		if (hasLatestStable) {
+			items.unshift(LATEST_STABLE);
+		}
 		return items;
 	}
 
@@ -52,7 +69,7 @@ function sort(items, isSemverItems) {
 
 function toDeployLinks(prefix, suffix, items, isSemverItems) {
 	items = sort(items, isSemverItems);
-	return items.map((item) => `<li><a href="${prefix}${item}${suffix}">${item}</a></li>`).join('\n');
+	return items.map((item) => `<li><a class="deploy-link" href="${prefix}${item}${suffix}" target="_blank" rel="noopener noreferrer">${item}</a></li>`).join('\n');
 }
 
 const STYLEGUIDE_URL_PREFIX = 'https://square.github.io/maker/styleguide/';
@@ -73,6 +90,18 @@ const BUILT_INDEX = `
 			margin: 0 0 0.5em 0;
 			white-space: nowrap;
 		}
+		.maker-link {
+			color: inherit;
+			font-family: inherit;
+			font-size: inherit;
+			font-weight: inherit;
+			text-decoration: inherit;
+			background-color: inherit;
+		}
+		.maker-link:hover {
+			color: white;
+			background-color: black;
+		}
 		.categories, .subcategories {
 			display: flex;
 			flex-wrap: wrap;
@@ -80,28 +109,51 @@ const BUILT_INDEX = `
 		}
 		.categories {
 			gap: 32px;
+			margin-bottom: 16px;
 		}
-		ul {
+		.deploy-links {
 			margin: 0;
 			padding: 0;
 			list-style-type: none;
+			font-family: monospace;
+			font-size: 16px;
+		}
+		.deploy-link {
+			text-decoration: none;
+			color: blue;
+			background-color: transparent;
+		}
+		.deploy-link:visited {
+			color: purple;
+		}
+		.deploy-link:hover {
+			color: white;
+			background-color: blue;
+		}
+		.deploy-link:hover:visited {
+			background-color: purple;
+		}
+		.updated-msg {
+			font-style: italic;
 		}
 	</style>
 </head>
 <body>
+	<h1><a class="maker-link" href="https://github.com/square/maker" target="_blank" rel="noopener noreferrer">Maker</a> Deploys 🚀</h1>
+
 	<div class="categories">
 		<div class="category">
 			<h2>Styleguides</h2>
 			<div class="subcategories">
 				<div class="subcategory">
 					<h3>Releases</h3>
-					<ul>
+					<ul class="deploy-links">
 						${toDeployLinks(STYLEGUIDE_URL_PREFIX, URL_SUFFIX, STYLEGUIDE_DEPLOYS.filter(isVersion), true)}
 					</ul>
 				</div>
 				<div class="subcategory">
-					<h3>WIPs</h3>
-					<ul>
+					<h3>PRs</h3>
+					<ul class="deploy-links">
 						${toDeployLinks(STYLEGUIDE_URL_PREFIX, URL_SUFFIX, STYLEGUIDE_DEPLOYS.filter(isntVersion))}
 					</ul>
 				</div>
@@ -113,19 +165,30 @@ const BUILT_INDEX = `
 			<div class="subcategories">
 				<div class="subcategory">
 					<h3>Releases</h3>
-					<ul>
+					<ul class="deploy-links">
 						${toDeployLinks(LAB_URL_PREFIX, URL_SUFFIX, LAB_DEPLOYS.filter(isVersion), true)}
 					</ul>
 				</div>
 				<div class="subcategory">
-					<h3>WIPs</h3>
-					<ul>
+					<h3>PRs</h3>
+					<ul class="deploy-links">
 						${toDeployLinks(LAB_URL_PREFIX, URL_SUFFIX, LAB_DEPLOYS.filter(isntVersion))}
 					</ul>
 				</div>
 			</div>
 		</div>
 	</div>
+
+	<span class="updated-msg">
+		Last updated on <span class="updated-time"></span>
+	</span>
+
+	<script>
+		const lastUpdated = new Date(${Date.now()});
+		document
+			.querySelector('.updated-time')
+			.innerText = lastUpdated.toLocaleString();
+	</script>
 </body>
 </html>
 `;
--- build/utils.js
+++ build/utils.js
@@ -34,6 +34,9 @@ const SEMANTIC_BRANCHES = new Set([
 const STABLE_BRANCHES = new Set([
 	'master',
 ]);
+const BETA_BRANCHES = new Set([
+	'beta',
+]);
 const PREVIEW_BRANCHES = new Set([
 	'next',
 	'next-major',
@@ -41,7 +44,7 @@ const PREVIEW_BRANCHES = new Set([
 	'alpha',
 ]);
 
-function isSemanticReleaseBranch(branchName) {
+function isSemanticRelease(branchName) {
 	return SEMANTIC_BRANCHES.has(branchName) || BACKPORT_BRANCH_REGEX.test(branchName);
 }
 
@@ -49,20 +52,26 @@ function isStableRelease(branchName) {
 	return STABLE_BRANCHES.has(branchName) || BACKPORT_BRANCH_REGEX.test(branchName);
 }
 
+function isBetaRelease(branchName) {
+	return BETA_BRANCHES.has(branchName);
+}
+
 function isPreviewRelease(branchName) {
 	return PREVIEW_BRANCHES.has(branchName);
 }
 
+const NO_VERSION_INFO = '0.0.0-semantic-release';
+
 function getLibraryVersion() {
 	const zero = 0;
 	let libraryVersion = version;
-	if (libraryVersion === '0.0.0-semantic-release') {
+	if (libraryVersion === NO_VERSION_INFO) {
 		let tagsOutput = execSync('git tag --points-at HEAD'); // returns byte buffer
 		tagsOutput = tagsOutput.toString(); // converts byte buffer to string
 		const semverTags = tagsOutput
 			.trim()
-			.split(/(\s+)/)
-			.filter((tag) => tag.trim().length > zero)
+			.split(/(\s+)/) // split by whitespace
+			.filter((tag) => tag.trim().length > zero) // remove empty strings
 			.map((tag) => semver.clean(tag)) // may return null
 			.filter((tag) => !!tag); // null check
 		semverTags.sort((tagA, tagB) => {
@@ -77,14 +86,43 @@ function getLibraryVersion() {
 			[libraryVersion] = semverTags; // latest semver tag
 		}
 	}
+	// will return 0.0.0-semantic-release if no tag found at HEAD
+	// which will be the case on commit types like: ci, docs, refactor
 	return libraryVersion;
 }
 
+function getDeployName() {
+	const branchName = getCurrentBranch();
+
+	if (process.env.DEPLOY_NAME === 'git_branch') {
+		return branchName;
+	}
+	if (!isSemanticRelease(branchName)) {
+		return branchName;
+	}
+	const libraryVersion = getLibraryVersion();
+	if (process.env.DEPLOY_NAME === 'library_version') {
+		return libraryVersion;
+	}
+	if (libraryVersion === NO_VERSION_INFO) {
+		return branchName;
+	}
+	return libraryVersion;
+}
+
+const LATEST_STABLE = 'latest-stable';
+const LATEST_BETA = 'latest-beta';
+
 module.exports = {
+	LATEST_STABLE,
+	LATEST_BETA,
+	NO_VERSION_INFO,
 	merge,
+	getDeployName,
 	getCurrentBranch,
 	getLibraryVersion,
-	isSemanticReleaseBranch,
+	isSemanticRelease,
 	isStableRelease,
 	isPreviewRelease,
+	isBetaRelease,
 };
