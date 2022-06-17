96a843b32f88015eb0744d7f9a536de7fd66e48e
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2022-06-17 06:31:32 -0400

    docs: adds lab to show how to theme components to look discount-y

---
+++ lab/experiments/Discounts.vue
@@ -0,0 +1,285 @@
+<template>
+	<m-theme
+		class="demo"
+		:theme="discountTheme"
+		:style="inlineStyles"
+	>
+		<div class="theme-controls">
+			<label>
+				<m-text pattern="title">
+					primary color
+				</m-text>
+				<input
+					v-model="primaryColor"
+					type="color"
+				>
+			</label>
+			<m-text pattern="title">
+				discounts theme
+			</m-text>
+			<pre>{{ JSON.stringify(discountTheme, null, 2) }}</pre>
+		</div>
+		<div class="theme-preview">
+			<m-text pattern="title">
+				discount icons
+			</m-text>
+			<div class="icons">
+				<m-icon name="discount" />
+				<div class="blocky-discount-icon">
+					<m-icon name="discount" />
+				</div>
+			</div>
+			<m-text pattern="title">
+				discount pills
+			</m-text>
+			<div class="pills">
+				<m-pill pattern="discount">
+					BUY 1, GET 10% OFF
+				</m-pill>
+				<m-pill pattern="discount">
+					BUY 3, GET 50% OFF
+				</m-pill>
+			</div>
+			<m-text pattern="title">
+				discount notices
+			</m-text>
+			<div class="notices">
+				<m-notice
+					pattern="discount"
+					variant="inline"
+				>
+					BUY 1, GET 10% OFF (inline)
+					<template #actions>
+						<m-text-button pattern="discount">
+							Redeem
+						</m-text-button>
+					</template>
+				</m-notice>
+				<m-notice
+					pattern="discount"
+					variant="block"
+				>
+					BUY 3, GET 50% OFF (block)
+					<template #actions>
+						<m-text-button pattern="discount">
+							Redeem
+						</m-text-button>
+					</template>
+				</m-notice>
+			</div>
+			<m-text pattern="title">
+				discount cards
+			</m-text>
+			<div class="cards">
+				<m-card class="discount-card">
+					<div class="discount-details">
+						<m-text
+							pattern="title"
+							font-weight="600"
+							:size="-1"
+						>
+							30% off Wood Incense Burners
+						</m-text>
+						<m-text
+							:size="-2"
+						>
+							Expires Tomorrow
+						</m-text>
+					</div>
+					<div
+						class="blocky-discount-icon justify-end-self"
+					>
+						<m-icon name="discount" />
+					</div>
+					<m-text-button
+						class="align-end-self"
+						pattern="discount"
+						style="padding: 0;"
+					>
+						View item
+					</m-text-button>
+					<m-text-button
+						class="align-end-self justify-end-self"
+						pattern="discount"
+						style="padding: 0;"
+					>
+						View info
+					</m-text-button>
+				</m-card>
+				<m-card class="discount-card">
+					<div class="discount-details">
+						<m-text
+							pattern="title"
+							font-weight="600"
+							:size="-1"
+						>
+							20% off select Bouquets with purchase of 1 Vase
+						</m-text>
+						<m-text
+							:size="-2"
+						>
+							Discount display in cart
+						</m-text>
+					</div>
+					<div
+						class="blocky-discount-icon justify-end-self"
+					>
+						<m-icon name="discount" />
+					</div>
+					<m-text-button
+						class="align-end-self"
+						pattern="discount"
+						style="padding: 0;"
+					>
+						View items
+					</m-text-button>
+					<m-text-button
+						class="align-end-self justify-end-self"
+						pattern="discount"
+						style="padding: 0;"
+					>
+						View info
+					</m-text-button>
+				</m-card>
+			</div>
+		</div>
+	</m-theme>
+</template>
+
+<script>
+import chroma from 'chroma-js';
+import { MTheme } from '@square/maker/components/Theme';
+import { MPill } from '@square/maker/components/Pill';
+import { MNotice } from '@square/maker/components/Notice';
+import { MCard } from '@square/maker/components/Card';
+import { MText } from '@square/maker/components/Text';
+import { MIcon } from '@square/maker/components/Icon';
+import { MTextButton } from '@square/maker/components/TextButton';
+import ZapIcon from '@square/maker-icons/Zap';
+
+export default {
+	components: {
+		MTheme,
+		MPill,
+		MNotice,
+		MText,
+		MTextButton,
+		MCard,
+		MIcon,
+	},
+	data() {
+		return {
+			primaryColor: '#006AFF', // blue
+		};
+	},
+	computed: {
+		primarySubtleColor() {
+			const BRIGHTEN = 2.5;
+			return chroma(this.primaryColor).brighten(BRIGHTEN).hex();
+		},
+		inlineStyles() {
+			return {
+				'--color-primary-subtle': this.primarySubtleColor,
+			};
+		},
+		discountTheme() {
+			return {
+				colors: {
+					primary: this.primaryColor,
+					primarySubtle: this.primarySubtleColor,
+				},
+				icons: {
+					discount: ZapIcon,
+				},
+				pill: {
+					patterns: {
+						discount: {
+							textColor: '@colors.primary',
+							bgColor: '@colors.primarySubtle',
+						},
+					},
+				},
+				notice: {
+					patterns: {
+						discount: {
+							iconName: 'discount',
+							iconColor: '@colors.primary',
+							color: '@colors.primary',
+							bgColor: '@colors.primarySubtle',
+						},
+					},
+				},
+				textbutton: {
+					patterns: {
+						discount: {
+							color: '@colors.primary',
+						},
+					},
+				},
+			};
+		},
+	},
+};
+</script>
+
+<style scoped>
+.demo {
+	display: flex;
+	flex-wrap: wrap;
+	gap: 16px;
+	padding: 16px;
+	font-family: arial, sans-serif;
+}
+
+.theme-controls,
+.theme-preview {
+	display: block;
+	width: 400px;
+}
+
+.blocky-discount-icon {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	width: 48px;
+	height: 48px;
+	color: var(--maker-color-primary);
+	background-color: var(--color-primary-subtle);
+	border-radius: var(--maker-shape-default-border-radius);
+}
+
+.discount-card {
+	display: grid;
+	grid-template-rows: 1fr auto;
+	grid-template-columns: 1fr auto;
+	row-gap: 8px;
+	column-gap: 8px;
+}
+
+.justify-end-self {
+	justify-self: end;
+}
+
+.align-end-self {
+	align-self: end;
+}
+
+label {
+	cursor: pointer;
+}
+
+input {
+	cursor: pointer;
+}
+
+.icons {
+	color: var(--maker-color-primary);
+}
+
+.icons > *,
+.pills > *,
+.notices > *,
+.cards > * {
+	margin: 0 8px 8px 0;
+}
+</style>
