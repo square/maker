8ffbe4d97505f9d5341115fb282e3abc1203609e
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2022-07-26 09:09:05 -0400

    feat: adds emotion/css (to enable class tagging later hopefully)

---
+++ lab/experiments/ClassTagging.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="container">
+		hello world
+	</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.container {
+	display: block;
+}
+</style>
