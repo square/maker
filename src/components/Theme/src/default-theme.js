f983e5b18eabd400e3720b7dd5f24a2188bf38e7
kirill <7769424+pretzelhammer@users.noreply.github.com>
2022-08-23 08:13:42 -0400

    feat: makes progressbar themeable (#406)

--- src/components/Theme/src/default-theme.js
+++ src/components/Theme/src/default-theme.js
@@ -286,6 +286,11 @@ export default function defaultTheme() {
 			iconColor: '@colors.contextualPrimary.fill',
 			iconName: undefined,
 		},
+		progressbar: {
+			color: '@colors["neutral-100"]',
+			size: 'medium',
+			shape: undefined,
+		},
 		modal: {
 			color: undefined,
 			bgColor: undefined,
