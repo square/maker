1651c7770e120f9e29ba3c6e2c654cbcbcabe01d
pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>
2022-09-09 11:26:25 -0400

    feat: makes bread stay open if mouse is hovering over it

--- src/components/Toast/src/Bread.vue
+++ src/components/Toast/src/Bread.vue
@@ -2,7 +2,7 @@
 	<div
 		:class="$s.Bread"
 		v-bind="$attrs"
-		v-on="$listeners"
+		v-on="listeners"
 	>
 		<slot />
 	</div>
@@ -39,13 +39,71 @@ export default {
 		},
 	},
 
+	data() {
+		return {
+			mouseEnterAt: 0,
+			mouseLeaveAt: 0,
+			closeQueued: false,
+		};
+	},
+
+	computed: {
+		listeners() {
+			const listeners = {
+				// apply listeners set by parent
+				...this.$listeners,
+			};
+			if (this.persistent) {
+				return listeners;
+			}
+			// if this is an auto-dismiss bread then also
+			// conditionally apply mouse event listeners
+			listeners['&mouseenter'] = this.onMouseEnter;
+			listeners['&mouseleave'] = this.onMouseLeave;
+			// vue protip: prefixed the event name with
+			// an & tells vue to apply the passive modifier
+			return listeners;
+		},
+	},
+
 	mounted() {
 		if (this.persistent) {
 			return;
 		}
-		setTimeout(() => {
-			this.toastApi.closeSelf();
-		}, this.dismissAfter);
+		this.closeSelfIn(this.dismissAfter);
+	},
+
+	methods: {
+		onMouseEnter() {
+			this.mouseEnterAt = Date.now();
+		},
+		onMouseLeave() {
+			this.mouseLeaveAt = Date.now();
+			// if toast is still within its open duration
+			// window then don't do anything, let the
+			// current timeout resolve the close action
+			if (!this.closeQueued) {
+				return;
+			}
+			const ONE_SECOND = 1000;
+			this.closeSelfIn(ONE_SECOND);
+		},
+		closeSelfIn(durationMs) {
+			this.closeQueued = false;
+			setTimeout(() => {
+				// if mouse never hovered over bread, or if it
+				// hovered over bread but left already then
+				// close immediately
+				if (this.mouseLeaveAt >= this.mouseEnterAt) {
+					this.toastApi.closeSelf();
+					return;
+				}
+				// if mouse is currently hovering over bread
+				// then queue up a close action which will
+				// be resolved when the mouse leaves
+				this.closeQueued = true;
+			}, durationMs);
+		},
 	},
 };
 </script>
