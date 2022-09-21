13b31953a298dac62a9e1affb7b22e74141abc43
laurenhu <ruiwen@squareup.com>
2022-09-21 09:01:48 -0400

    feat(choice): choice card (#430)
    * fix: color fallback values
    
    * fix: normalizes fallback values for all public theme css vars, creates single source of truth
    
    * fix: theme-less blades, dialogs, modals now open
    
    * fix(theme): makes heading & body colors explicit hex values
    
    * docs(choice): makes first example slightly better
    
    * docs(popover): fixes bugs in popover docs
    
    * docs(progresscircle): fixes bug in progresscircle docs
    
    * docs(skeleton): improves skeleton docs
    
    * docs: fixes theme profile bug in docs
    
    * fix(toggle): invalid state styles (#427)
    
    * docs: removes unnecessary id from profiles in popover docs example
    
    * feat(choice): add choice card option
    
    * feat(choice): add choice card slot prop selected
    
    Co-authored-by: pretzelhammer <7769424+pretzelhammer@users.noreply.github.com>

--- src/components/Choice/index.js
+++ src/components/Choice/index.js
@@ -1,2 +1,3 @@
 export { default as MChoice } from './src/Choice.vue';
 export { default as MChoiceOption } from './src/ChoiceOption.vue';
+export { default as MChoiceCard } from './src/ChoiceCard.vue';
---
+++ src/components/Choice/src/ChoiceCard.vue
@@ -0,0 +1,86 @@
+<template>
+	<m-card
+		:class="[
+			$s.ChoiceCard,
+			{
+				[$s.selected]: isSelected,
+				[$s.disabled]: isDisabled,
+			},
+		]"
+		:disabled="isDisabled"
+		@click="selectSelf"
+	>
+		<!-- @slot has `selected` prop -->
+		<slot
+			:selected="isSelected"
+		/>
+	</m-card>
+</template>
+
+<script>
+import { MCard } from '@square/maker/components/Card';
+import key from './key';
+
+export default {
+	components: {
+		MCard,
+	},
+
+	inject: {
+		controlState: key,
+	},
+
+	props: {
+		value: {
+			type: undefined,
+			required: true,
+		},
+
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+	},
+
+	computed: {
+		isSelected() {
+			return this.controlState.isSelected(this.value);
+		},
+
+		isDisabled() {
+			return this.disabled || this.controlState.disabled;
+		},
+	},
+
+	methods: {
+		selectSelf() {
+			if (!this.isDisabled) {
+				this.controlState.selectValue(this.value);
+			}
+		},
+	},
+};
+</script>
+
+<style module="$s">
+/*
+	Defined in Choice component:
+	--selected-color
+	--selected-contrast-color
+	--selected-contrast-color-40
+*/
+.ChoiceCard {
+	width: 100%;
+	cursor: pointer;
+
+	&.selected {
+		border-color: var(--selected-color, $maker-color-neutral-90);
+		box-shadow: 0 0 0 1px var(--selected-color, $maker-color-neutral-90) inset;
+	}
+
+	&.disabled {
+		cursor: not-allowed;
+		opacity: 0.5;
+	}
+}
+</style>
