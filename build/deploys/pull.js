6b37984c4fd046ae7668235ab95ab7cbe243ac23
kirill <7769424+pretzelhammer@users.noreply.github.com>
2022-09-28 16:37:39 -0400

    ci: increase deploy styleguide & lab job success rate (#443)
    * ci: increase publish styleguide & lab job success rate
    
    * ci: improves Maker Deploys page
    
    * ci: remove rocket emoji from ci message
    
    * ci: improve speed & maker deploys page
    
    * ci: improves maker deploys page styling
    
    * style: fix spacing in package.json

--- build/deploys/pull.js
+++ build/deploys/pull.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-console */
+
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 const ensureDeployDirectory = require('./ensure');
+
+const EXIT_ERROR_CODE = 1;
 
 (async function pullDeploys() {
 	const deployDirectory = await ensureDeployDirectory();
 	process.chdir(deployDirectory);
 
 	// pull
-	await exec('git pull');
-}());
+	await exec('git pull --rebase');
+}()).catch((error) => {
+	console.error(error.message);
+	process.exit(EXIT_ERROR_CODE);
+});
