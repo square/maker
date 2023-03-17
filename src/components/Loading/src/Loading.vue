288e72b1c85ec6176a819adca8bcf50127865cf9
kirill <7769424+pretzelhammer@users.noreply.github.com>
2023-03-17 15:00:50 -0400

    refactor!: standardize sizes between MIcon & MLoading (#538)
    * refactor!: standardize sizes between MIcon & MLoading
    
    * docs: code comments

--- src/components/Loading/src/Loading.vue
+++ src/components/Loading/src/Loading.vue
@@ -1,17 +1,27 @@
 <template>
 	<div :class="$s.Loading">
 		<m-icon
-			:class="[
-				$s.Spinner,
-				$s[`size_${size}`],
-			]"
+			:class="$s.Spinner"
+			:size="size"
 			name="spinner"
 		/>
 	</div>
 </template>
 
 <script>
+import cssValidator from '@square/maker/utils/css-validator';
 import { MIcon } from '@square/maker/components/Icon';
+
+const ICON_SIZES = new Set([
+	'small',
+	'medium',
+	'large',
+	'xlarge',
+	'xxlarge',
+]);
+
+// width & height css props accept same set of values
+const sizeValidator = cssValidator('width');
 
 /**
  * @inheritAttrs div
@@ -23,10 +33,14 @@ export default {
 	},
 
 	props: {
+		/**
+		 * size of spinner icon, can be named value or any valid CSS width/height
+		 * @values small, medium, large, xlarge, xxlarge
+		 */
 		size: {
 			type: String,
-			default: 'medium',
-			validator: (size) => ['medium', 'large'].includes(size),
+			default: 'small',
+			validator: (size) => ICON_SIZES.has(size) || sizeValidator(size),
 		},
 	},
 };
@@ -46,15 +60,5 @@ export default {
 
 .Spinner {
 	animation: spin-one-turn 1s linear infinite;
-}
-
-.size_medium {
-	width: 16px;
-	height: 16px;
-}
-
-.size_large {
-	width: 64px;
-	height: 64px;
 }
 </style>
